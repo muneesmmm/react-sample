c87d5bdaae75f1f1e18357248df6ea7e7edf08ea
Muhammed Munees <muneesmmm@gmail.com>
2021-08-05 08:59:21 +0530

    update About page

--- src/components/Aboutus/Aboutus.js
+++ src/components/Aboutus/Aboutus.js
@@ -10,7 +10,7 @@ function Aboutus() {
                     </div>
             </div>
             <div className="row justify-content-center ">
-                    <div className="col-md-6 order-1 p-3 mt-5" data-aos="zoom-out-right" data-aos-duration="1000"  >
+                    <div className="col-md-6 order-1 p-3 mt-5" data-aos="zoom-out-top" data-aos-duration="1000"  >
                         
                         <p style={{ textAlign: "justify" }}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -25,7 +25,7 @@ function Aboutus() {
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                         cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
-                    <div className="col-md-6 order-2" data-aos="zoom-out-left" data-aos-duration="3000"  >
+                    <div className="col-md-6 order-2" data-aos="zoom-out-bottom" data-aos-duration="3000"  >
                         <img className="p-2" src={'https://web-peppers.com/wp-content/uploads/2020/02/How-to-Hire-the-Best-Dedicated-Development-Team-768x469.jpg'} alt='web' style={{ width: "100%" }}></img>
                     </div>
                 </div>
